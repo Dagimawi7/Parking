000af07c80222b58feb548ca6288fbc778894ad9
Dagimawi7 <20dtamrat@gmail.com>
2026-03-01 22:46:06 -0500

    fixed all the footer elemtns to sit in their designed place

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatMenuModule} from '@angular/material/menu';
+import { FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatMenuModule],
+  imports: [RouterOutlet, MatMenuModule, FontAwesomeModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
 export class App {
   protected readonly title = signal('Parking');
+  
 }
--- src/app/layout/home/component/footer-component/footer-component.ts
+++ src/app/layout/home/component/footer-component/footer-component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
-  selector: 'app-footer-component',
-  imports: [],
+  selector: 'app-footer-component', 
+  imports: [MatIconModule,FontAwesomeModule],
   templateUrl: './footer-component.html',
   styleUrl: './footer-component.css',
 })
 export class FooterComponent {
-
+  faXTwitter = faXTwitter;
+  faInstagram = faInstagram;
+  faFacebook = faFacebook;
 }
